ac54507a98980257a18e68f48da0621910b8694e
Vitor Boccio <vitorboccio@gmail.com>
2025-03-26 15:48:48 -0600

    facelift

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+const props = {
+  title: "Request a demo",
+  buy_link: `https://buy.stripe.com/aEU5kl5YhaB92bKbIK`,
+  free_link: `https://app.useflagpole.dev/request-demo`,
+  creatorIg: `https://www.instagram.com/codetonero`,
+};
+
+export default props;
